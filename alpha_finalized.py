bd747547de58f1d2a2b5f1ac89121b29b597c528
πάντα ῥεῖ <51081602+ChangeDEVOPS@users.noreply.github.com>
2020-04-19 15:04:35 +0300

    Update alpha_finalized.py

--- alpha_finalized.py
+++ alpha_finalized.py
@@ -24,7 +24,8 @@ query_data = requests.get \
     ((f'https://fmpcloud.io/api/v3/stock-screener?sector={sector}&betaMoreThan={beta_l}&betaLessThan={beta_m}\
     &marketCapMoreThan={capitalization_l}&marketCapLessThan={capitalization_m}&limit={number_stocks}&apikey={API_key}'))
 
-query_data = query_data.json()  # convert to readable format
+query_data = query_data.json() # convert to readable format
+
 print()
 print("Preliminary portfolio has", len(query_data), "stocks.")
 print()
@@ -55,51 +56,63 @@ while True:
         pos += 1
 
 final_portfolio = []
+print()
 
 max_priceEarningsRatio= float(input\
     ("Enter maximum value for PE Ratio\n\
-    https://www.investopedia.com/terms/p/price-earningsratio.asp (e.g 50) (min == 0,default): "))
+    https://www.investopedia.com/terms/p/price-earningsratio.asp (e.g 100) (min == 0,default): "))
 max_priceToBookRatio = float(input\
     ("Enter maximum value for PB Ratio \n\
-    https://www.investopedia.com/terms/p/price-to-bookratio.asp (e.g 50) (min == 0,default): "))
+    https://www.investopedia.com/terms/p/price-to-bookratio.asp (e.g 100) (min == 0,default): "))
 min_returnOnEquity = float(input\
     ("Enter minimum value for ROE\n\
-    https://www.investopedia.com/terms/r/returnonequity.asp (e.g 0.05) (min == 0,default): "))
+    https://www.investopedia.com/terms/r/returnonequity.asp (e.g 0.01) (min == 0,default): "))
 min_netProfitMargin = float(input\
     ("Enter minimum value for Profit Margin\n\
-    https://www.investopedia.com/terms/p/profitmargin.asp (e.g 0.05) (min == 0,default): "))
+    https://www.investopedia.com/terms/p/profitmargin.asp (e.g 0.01) (min == 0,default): "))
+print()
 
 for item in query_data:
     stock_ticker = item['symbol']
-    stock_ticker = str(stock_ticker).replace(".","")
-    query_ratios = requests.get \
-        ((f'https://fmpcloud.io/api/v3/ratios/FB?apikey={API_key}'))
-    query_ratios = query_ratios.json()
-    current_year_ratios = query_ratios[0].items()
+    try:
+        query_ratios = requests.get\
+            ((f'https://fmpcloud.io/api/v3/ratios/{stock_ticker}?apikey={API_key}'\
+              .format(stock_ticker=stock_ticker, API_key=API_key))) # HERE I HAVE ISSUE THE QUERY DOESN'T WORK FOR ALL TICKER - JUST "FB"
+        query_ratios = query_ratios.json()
+    #print(query_ratios)
+        current_year_ratios = query_ratios[0].items()
     # print()
     # print(current_year_ratios)
 
-    PE = None
-    PB = None
-    ROE = None
-    PM = None
 
-    for (k, v) in current_year_ratios:
-        if k == "priceEarningsRatio":
-            PE = float(v)
-        if k == "priceToBookRatio":
-            PB = float(v)
-        if k == "returnOnEquity":
-            ROE = float(v)
-        if k == "netProfitMargin":
-            PM = float(v)
+        PB = None
+        ROE = None
+        PM = None
+        PE = None
 
-    if PE < max_priceEarningsRatio\
-            and PB < max_priceToBookRatio\
-            and ROE > min_returnOnEquity\
-            and PM > min_netProfitMargin:
+        for (k, v) in current_year_ratios:
+            if k == "priceEarningsRatio":
+                PE = float(v)
+            if k == "priceToBookRatio":
+                PB = float(v)
+            if k == "returnOnEquity":
+                ROE = float(v)
+            if k == "netProfitMargin":
+                PM = float(v)
 
-        final_portfolio.append(item)
+        if PE < max_priceEarningsRatio\
+                and PB < max_priceToBookRatio\
+                and ROE > min_returnOnEquity\
+                and PM > min_netProfitMargin:
+
+            final_portfolio.append(item)
+            print("Ticker:", item['symbol'], "-", item['companyName']," has PE of", round(PE,4))
+            print("Ticker:", item['symbol'], "-", item['companyName']," has PB of", round(PB,4))
+            print("Ticker:", item['symbol'], "-", item['companyName']," has ROE of", round(ROE,4))
+            print("Ticker:", item['symbol'], "-", item['companyName']," has PM of", round(PM,4))
+            print()
+    except:
+        pass
 
 print()
 print("Finalized portfolio has", len(final_portfolio), "stocks.")
